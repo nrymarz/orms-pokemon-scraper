2ad9e261a25d5cf20ca9ec5efd20786571e98786
nrymarz <nrymarz@gmail.com>
2020-11-19 13:43:48 -0600

    commit tree 3707c85fb17f7e0c993e5699b3aff8162f7ee1b6

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
 class Pokemon
+    attr_accessor :name,:type,:db, :id
+    def initialize(name:,type:,id:nil,db:)
+        @name = name
+        @type = type
+        @db = db
+        @id = id
+    end
+
+    def self.save(name,type,db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name,type) VALUES (?,?)
+        SQL
+        db.execute(sql,name,type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id,db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        row = db.execute(sql,id)[0]
+        self.new(name:row[1],type:row[2],id:row[0],db:db)
+    end
 end
